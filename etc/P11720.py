d8d1d31512eb9579672eb1b6381649f3bf867851
xoxowo <1qq11212@gmail.com>
2022-10-16 14:27:50 +0900

    001 예제 실행결과 주석

--- etc/P11720.py
+++ etc/P11720.py
@@ -6,3 +6,10 @@ for i in numbers :
     sum += int(i)
 
 print(sum)
+
+"""
+실행결과 값
+1 -> n = input() 의미없는 행 
+23456 -> numbers = list(input()) 두번째줄에 숫자 입력하면 list로 아래 for문돌아서 아래 결과값 출력
+20 -> print(sum)
+"""
